efa7728c1ff365671cdc03ac11b71f7251155623
Manuel Aldir <mannyaldir@gmail.com>
2026-06-17 04:36:21 -0400

    Create account registration page routed in Flask

--- app.py
+++ app.py
@@ -24,6 +24,10 @@ def login():
     else:
         print('No POST info')
     return render_template('login.html')
+@app.route('/register', methods=['GET','POST'])
+def register():
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
