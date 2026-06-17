00ef684cb673f22f608f69a694b57ee05df10756
Manuel Aldir <mannyaldir@gmail.com>
2026-06-17 02:12:07 -0400

    created sqlalchemy models for user,project,milestone and task

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 # import dependancy
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create a Flask object using file name as argument
 app = Flask(__name__) 
 
-@app.route('/')
-@app.route('/login') # decorator that creates directory path for function
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/login', methods=['GET','POST']) # decorator that creates directory path for function
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        print(email,password)
+    else:
+        print('No POST info')
     return render_template('login.html')
 
 if __name__ == '__main__':
---
+++ models.py
@@ -0,0 +1,50 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from datetime import datetime, timezone
+from typing import Optional
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+class User(db.Model):
+    userId: Mapped[int] = mapped_column(primary_key=True)
+    firstName:Mapped[str] = mapped_column(nullable=False)
+    lastName:Mapped[str] = mapped_column(nullable=False)
+    passwordHash:Mapped[str] = mapped_column(nullable=False)
+    email:Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    createdAt:Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc) ,nullable=False)
+
+class Project(db.Model):
+    projectId: Mapped[int] = mapped_column(primary_key=True)
+    userId: Mapped[int] = mapped_column(ForeignKey('user.userId'), nullable=False)
+    projectName:Mapped[str] = mapped_column(nullable=False)
+    description:Mapped[str] = mapped_column(nullable=True)
+    startDate: Mapped[datetime] = mapped_column(nullable=True)
+    status: Mapped[str] = mapped_column(nullable=False)
+    createdAt:Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc) ,nullable=False)
+
+class Milestone(db.Model):
+    milestoneId: Mapped[int] = mapped_column(primary_key=True)
+    projectId: Mapped[int] = mapped_column(ForeignKey('project.projectId'), nullable=False)
+    milestoneName:Mapped[str] = mapped_column(nullable=False)
+    description:Mapped[str] = mapped_column(nullable=True)
+    startDate: Mapped[datetime] = mapped_column(nullable=False)
+    endDate: Mapped[datetime] = mapped_column(nullable=False)
+    status: Mapped[str] = mapped_column(nullable=False)
+    createdAt:Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc) ,nullable=False)
+
+class Task(db.Model):
+    taskId: Mapped[int] = mapped_column(primary_key=True)
+    projectId: Mapped[int] = mapped_column(ForeignKey('project.projectId'), nullable=False)
+    milestoneId: Mapped[Optional[int]] = mapped_column(ForeignKey('milestone.milestoneId'), nullable=True)
+    taskName:Mapped[str] = mapped_column(nullable=False)
+    description:Mapped[str] = mapped_column(nullable=True)
+    dueDate: Mapped[datetime] = mapped_column(nullable=True)
+    status: Mapped[str] = mapped_column(nullable=False)
+    createdAt:Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc),nullable=False)
+
+
+
