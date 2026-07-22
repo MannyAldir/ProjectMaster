a12e7f27ff348dc7fabddee03c9702c232540815
Manuel Aldir <mannyaldir@gmail.com>
2026-07-22 16:27:58 -0400

    Create registration form with first name, last name, email, password, confirm password fields

---
+++ validation-forms/registration.py
@@ -0,0 +1,54 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, PasswordField
+from wtforms.validators import DataRequired,  Length, EqualTo, Email
+
+def strip_whitespace(value:str)->str | None:
+    if value:
+        return value.strip()
+    return value
+
+class Registration(FlaskForm):
+    first_name = StringField(
+        'First Name',
+        validators= [ 
+            DataRequired(), 
+            Length(max=25)
+        ],
+        filters=[strip_whitespace]
+    )
+
+    last_name = StringField(
+        'Last Name',
+        validators=[
+            DataRequired(),
+            Length(max=25)
+        ],
+        filters=[strip_whitespace]
+    )
+
+    email = EmailField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Email(message='Invalid email address'),
+            Length(min=6, max=35)
+        ],
+        filters=[strip_whitespace]
+    )
+
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            Length(min=8, max=35)
+        ]
+    )
+
+    confirm_password = PasswordField(
+        'Confirm Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password', message='Passwords must match.')
+
+        ]
+    )
